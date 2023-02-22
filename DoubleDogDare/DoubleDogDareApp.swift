5a010e02436b95c8629c0d7d2c58d560a2067d84
iWatger <ethanfox646@icloud.com>
2023-02-21 21:36:25 -0500

    Initial Commit

---
+++ DoubleDogDare/DoubleDogDareApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleDogDareApp.swift
+//  DoubleDogDare
+//
+//  Created by Ethan Fox on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DoubleDogDareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
